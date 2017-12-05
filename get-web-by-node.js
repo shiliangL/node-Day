899d6fde77a6297134a0a86e71cdaa2210cb61c6
shiliangl <shiliangwei@cloudolp.com>
2017-12-05 12:33:37 +0800

    code get data

--- get-web-by-node.js
+++ get-web-by-node.js
@@ -1,6 +1,40 @@
 const http = require("https");
+const cheerio = require("cheerio");
 
 const url = "https://www.imooc.com/learn/348";
+
+function filtersHtml(html) {
+  let courseData = [];
+
+  let $ = cheerio.load(html);
+  let chapters = $(".chapter");
+  chapters.each(function(item) {
+    let chapter = $(this);
+    let chapterTitle = chapter.find("strong").text(); //找到章节标题
+    let videos = chapter.find(".video").children("li");
+
+    let chapterData = {
+      chapterTitle: chapterTitle,
+      videos: []
+    };
+
+    videos.each(function(item) {
+      let video = $(this).find(".studyvideo");
+      let title = video.text();
+      let id = 0;
+
+      chapterData.videos.push({
+        title: title,
+        id: id
+      });
+    });
+
+    courseData.push(chapterData);
+  });
+
+  return courseData;
+}  
+
 
 http.get(url,(res)=>{
     let html = ''
@@ -9,6 +43,9 @@ http.get(url,(res)=>{
     })
 
     res.on('end',()=>{
-        console.log(html)
+        // console.log(html)
+        filtersHtml(html);
     })
+}).on('error',()=>{
+    console.log('获取失败')
 })
